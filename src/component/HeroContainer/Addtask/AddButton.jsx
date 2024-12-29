3b6ff3674558d3c1176b0c52765e1369debda238
yuneshdangol99 <uneshdangol99@gmail.com>
2024-12-29 22:05:16 +0545

    added add button ui

--- src/component/HeroContainer/Addtask/AddButton.jsx
+++ src/component/HeroContainer/Addtask/AddButton.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosAddCircleOutline } from "react-icons/io";
+import Showoverlay from "./component/Showoverlay";
 
-function AddButton() {
+function AddButton({ className }) {
+  const [ShowOverlay, setShowOverlay] = useState(false);
+
+  const handletask = () => {
+    setShowOverlay(true);
+  };
+
   return (
-    <button className="px-4 py-2 bg-primary rounded-xl text-white flex items-center gap-1">
-      <IoIosAddCircleOutline className="text-xl" /> Add Task
-    </button>
+    <>
+      <button
+        onClick={handletask}
+        className={`px-4 py-2 bg-primary rounded-xl text-white flex items-center gap-1`}
+      >
+        <IoIosAddCircleOutline className="text-xl" /> Add Task
+        <Showoverlay ShowOverlay={ShowOverlay} />
+      </button>
+    </>
   );
 }
 
